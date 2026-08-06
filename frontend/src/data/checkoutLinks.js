5f992dd2cba1f2f1b11f205f0021f4c89e611227
joaorodriguescasa2121-hash <joao.rodrigues.casa2121@gmail.com>
2026-08-06 00:00:23 -0300

    Updated checkoutLinks.js

--- frontend/src/data/checkoutLinks.js
+++ frontend/src/data/checkoutLinks.js
@@ -1,10 +1,10 @@
-// Cakto checkout links keyed by product slug. Each "Comprar agora" button
+// CartPanda checkout links keyed by product slug. Each "Comprar agora" button
 // opens the link for its corresponding iPhone model in a new tab.
 export const CHECKOUT_LINKS = {
-  "iphone-17": "https://pay.cakto.com.br/uta5sii_1020560",
-  "iphone-16": "https://pay.cakto.com.br/oi9fqqd_1020557",
-  "iphone-15": "https://pay.cakto.com.br/gigqo6k_1020555",
-  "iphone-14": "https://pay.cakto.com.br/3eokahu_1020554",
-  "iphone-13": "https://pay.cakto.com.br/3bt99oj_1020552",
-  "iphone-12": "https://pay.cakto.com.br/3f3ecfq_1020542",
+  "iphone-17": "https://paradisezzz.mycartpanda.com/checkout/211955870:1",
+  "iphone-16": "https://paradisezzz.mycartpanda.com/checkout/211956009:1",
+  "iphone-15": "https://paradisezzz.mycartpanda.com/checkout/211956015:1",
+  "iphone-14": "https://paradisezzz.mycartpanda.com/checkout/211956036:1",
+  "iphone-13": "https://paradisezzz.mycartpanda.com/checkout/211956037:1",
+  "iphone-12": "https://paradisezzz.mycartpanda.com/checkout/211956061:1",
 };
