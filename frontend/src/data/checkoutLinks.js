703ad7a0f381e0a7808470a2e8af1d43fed47988
joaorodriguescasa2121-hash <joao.rodrigues.casa2121@gmail.com>
2026-08-08 06:48:05 -0300

    Update checkout URLs for buy buttons

--- frontend/src/data/checkoutLinks.js
+++ frontend/src/data/checkoutLinks.js
@@ -1,10 +1,10 @@
-// Paradise Pags checkout links keyed by product slug. Each "Comprar agora" button
+// Checkout links keyed by product slug. Each "Comprar agora" button
 // opens the link for its corresponding iPhone model in a new tab.
 export const CHECKOUT_LINKS = {
-  "iphone-17": "https://multi.paradisepags.com/p/pix_0ecc66383428",
-  "iphone-16": "https://multi.paradisepags.com/p/pix_fb19e99892ec",
-  "iphone-15": "https://multi.paradisepags.com/p/pix_fbea27166f0b",
-  "iphone-14": "https://multi.paradisepags.com/p/pix_8377952f8c79",
-  "iphone-13": "https://multi.paradisepags.com/p/pix_2b380efb08c8",
-  "iphone-12": "https://multi.paradisepags.com/p/pix_f1fd9b645baf",
+  "iphone-17": "https://checkoutonline.org.ua/c/fceb8642de",
+  "iphone-16": "https://checkoutonline.org.ua/c/f6d5c24b74",
+  "iphone-15": "https://checkoutonline.org.ua/c/74187d6c4a",
+  "iphone-14": "https://checkoutonline.org.ua/c/eaf3caca66",
+  "iphone-13": "https://checkoutonline.org.ua/c/3f4a990e56",
+  "iphone-12": "https://checkoutonline.org.ua/c/17050ddbfc",
 };
